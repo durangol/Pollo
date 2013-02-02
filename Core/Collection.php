d31ebbfb1e569c4a2658247f61e13ad6f8c10990
njr1489 <njr1489@msn.com>
2013-02-01 21:40:54 -0500

    Fixing interface functions

--- Core/Collection.php
+++ Core/Collection.php
@@ -12,6 +12,10 @@ class Collection implements \Countable, \IteratorAggregate, \ArrayAccess
      */
     protected $_data = array();
 
+    /**
+     * Constructor
+     * @param array $data
+     */
     public function __construct(array $data = array())
     {
         foreach ($data as $key => $value) {
@@ -19,61 +23,89 @@ class Collection implements \Countable, \IteratorAggregate, \ArrayAccess
         }
     }
 
+    /**
+     * Returns the array
+     * @return array
+     */
     public function toArray()
     {
         return $this->_data;
     }
 
+    /**
+     * Implementing Countable
+     * @return integer
+     */
     public function count()
     {
         return count($this->_data);
     }
 
+    /**
+     * Returns current index of the iterator
+     * @return mixed
+     */
     public function current()
     {
         return current($this->_data);
     }
 
+    /**
+     * Implementing IteratorAggregate
+     * @return ArrayIterator
+     */
     public function getIterator()
     {
-        return new ArrayIterator($this->_elements);
+        return new \ArrayIterator($this->_data);
     }
 
+    /**
+     * Returns the key of the current element
+     * @return mixed
+     */
     public function key()
     {
         return key($this->_data);
     }
 
+    /**
+     * Moves the pointer to the next position
+     * @return mixed
+     */
     public function next()
     {
         return next($this->_data);
     }
 
-    public function rewind()
-    {
-        return rewind($this->_data);
-    }
-
-    public function valid()
-    {
-        return isset($this->_data);
-    }
-
+    /**
+     * Checks if the offset exists
+     * @return bool
+     */
     public function offsetExists($offset)
     {
         return isset($this->_data[$offset]);
     }
 
+    /**
+     * Gets the offset
+     * @return mixed
+     */
     public function offsetGet($offset)
     {
         return $this->_data[$offset];
     }
 
+    /**
+     * Sets a value at the offset position
+     */
     public function offsetSet($offset, $value)
     {
         $this->_data[$offset] = $value;
     }
 
+    /**
+     * Unsets an element at the offset position
+     */
     public function offsetUnset($offset)
     {
         unset($this->_data[$offset]);
