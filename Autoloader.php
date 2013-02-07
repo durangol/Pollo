14944b2588934eace5204420256d43c56a56b814
Luis M Durango <durangolm85@gmail.com>
2013-02-06 23:17:30 -0500

    start of autoloader and its interface

---
+++ Autoloader.php
@@ -0,0 +1,33 @@
+<?php
+require_once __DIR__ . '/Autoloader/AutoloaderInterface.php';
+namespace Pollo;
+
+class Autoloader
+{
+    protected $_autoloaders = array();
+
+    public function __construct()
+    {
+        spl_autoload_register(array($this, 'autoload'));
+    }
+
+    public function autoload($class)
+    {
+        foreach($this->_autoloaders as $autoloader) {
+            if ($autoloader instanceof \Pollo\Autoloado\Autoloader\Autoloaderinterface) {
+                $autoloader->autoload($class);
+            } 
+            else if (is_callable($autoloader)) {
+                $autoloader($class);
+            }
+        }
+    }
+
+    public function addAutoloader($autoloader)
+    {
+        if ($autoloader instanceof \Pollo\Autoloader\AutoloaderInterface || is_callable($autoloader)) {
+            throw new \InvalidArgumentException('The autoloader must be callable or an instance of \\Pollo\\Autoloader\\AutoloaderInterface.');
+        }
+        $this->_autoloaders[] = $autoloader;
+    }
+}
