901cf5b763a6d357a7b9953879e0ebbc167eb49a
Luis M Durango <durangolm85@gmail.com>
2013-02-16 16:58:12 -0500

    updated autoloader function addAutoloader to have two params and created the removeAutoloader function

--- Autoloader.php
+++ Autoloader.php
@@ -3,15 +3,35 @@
 namespace Pollo;
 require_once __DIR__ . '/Autoloader/AutoloaderInterface.php';
 
+/**
+ * A container for autoloaders that gets registered on the autoloader stack
+ */
 class Autoloader
 {
+    /**
+     * autoloader container
+     * @var array
+     */
     protected $_autoloaders = array();
 
-    public function __construct()
+    /**
+     * Optoinally adds autoloaders to the array and registers itself to the
+     * autoloader stack.
+     * @param array $autoloaders autoloader to add
+     */
+    public function __construct(array $autoloaders = array())
     {
+        foreach ($autoloaders as $key => $autoloader) {
+            $this->addAutoloader($key, $autoloader);
+        }
         spl_autoload_register(array($this, 'autoload'));
     }
 
+    /**
+     * checks each autoloader if its an instance of \Pollo\Autoloader\Autoloaderinterface
+     * or if its callable. 
+     * @param  string $class the path to the class you want to load 
+     */
     public function autoload($class)
     {
         foreach($this->_autoloaders as $autoloader) {
@@ -24,11 +44,25 @@ class Autoloader
         }
     }
 
-    public function addAutoloader($autoloader)
+    /**
+     * Add another autoloader
+     * @param string $key        the index of the autoloader array
+     * @param mixed $autoloader  its an instance of Autoloader Interface or a callable 
+     */
+    public function addAutoloader($key, $autoloader)
     {
         if (!$autoloader instanceof \Pollo\Autoloader\AutoloaderInterface && !is_callable($autoloader)) {
             throw new \InvalidArgumentException('The autoloader must be callable or an instance of \\Pollo\\Autoloader\\AutoloaderInterface.');
         }
-        $this->_autoloaders[] = $autoloader;
+        $this->_autoloaders[$key] = $autoloader;
+    }
+
+    /**
+     * removes Autoloader
+     * @param  string $key the index of the autoloader you want to remove
+     */
+    public function removeAutoloader($key)
+    {
+        unset($this->_autoloaders[$key]);
     }
 }
