f44848e0aa28d4f7772a2e84a748744e3b282657
Noel Ramos <njr1489@msn.com>
2013-02-23 18:11:30 -0500

    merge

--- Application/Mvc/Router.php
+++ Application/Mvc/Router.php
@@ -28,33 +28,47 @@ class Router implements RouterInterface
      * Adds a route to the array of routes
      * @param Pollo\Application\Mvc\Router\RouterInterface $route
      */
-    public function addRoute(RouterInterface $route)
+    public function addRoute($key, RouteInterface $route)
     {
-        $this->_routes[] = $route;
+        $this->_routes[$key] = $route;
     }
 
     /**
      * A wrapper for for addRoute. Does the same thing but for an array of routes.
+     * Adds an array of routes to the route collection
      * @param array $routes
      */
     public function addRoutes(array $routes)
     {
-        foreach($routes as $route) {
-            $this->addRoute($routes);
+        foreach($routes as $key => $route) {
+            $this->addRoute($key, $route);
         }
     }
 
     /**
-     * Removes a route
-     * @param Pollo\Application\Mvc\Router\Interface $route
+     * Removes a route by key
+     * @param string $key
      */
-    public function removeRoute(RouterInterface $route)
+    public function removeRoute($key)
     {
-
+        unset($this->_routes[$key]);
     }
 
-    public function matchRoute(RequestInterface $request)
+    /**
+     * Runs through the routes and sets the route object to the request on the first match
+     * @param  \Pollo\Request\Abstract $request
+     * @return bool
+     */
+    public function matchRoute(Request $request)
     {
-        
+        $foundRoute = false;
+        foreach ($this->_routes as $route) {
+            if ($route->match($request)) {
+                $request->setRoute($route);
+                $foundRoute = true;
+                break;
+            }
+        }
+        return $foundRoute;
     }
 }
