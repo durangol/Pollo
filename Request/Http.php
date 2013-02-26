7c564e70435967e2a44d6547d81d38a2befa0892
njr1489 <njr1489@msn.com>
2013-02-25 23:17:10 -0500

    Changes to HTTP Request, added HTTP Response class

--- Request/Http.php
+++ Request/Http.php
@@ -9,7 +9,6 @@ use \Pollo\Application\Mvc\Router\Route\RouteInterface;
  */
 class Http
 {
-
     /**
      * The route for the request
      * @var \Pollo\Application\Mvc\Router\Route\RouteInterface
@@ -34,21 +33,39 @@ class Http
         $this->_route = $route;
     }
 
-    public function get()
+    /**
+     * Returns the $_GET superglobal
+     * @return array
+     */
+    public function getParams()
     {
         return $_GET;
     }
 
+    /**
+     * Gets an element of $_GET with an index of $id
+     * @param string $id
+     * @return mixed
+     */
     public function getGetParam($id)
     {
         return isset($_GET[$id]) ? $_GET[$id] : null;
     }
 
+    /**
+     * Sets value to $_GET superglobal
+     * @param string $id
+     * @param mixed  $value
+     */
     public function setGetParam($id, $value)
     {
         $_GET[$id] = $value;
     }
 
+    /**
+     * Wrapper for setGetParam
+     * @param array $get
+     */
     public function setGetParams(array $get)
     {
         foreach ($get as $key => $value) {
@@ -56,21 +73,39 @@ class Http
         }
     }
 
+    /**
+     * Returns the $_POST superglobal
+     * @return array
+     */
     public function getPost()
     {
         return $_POST;
     }
 
+    /**
+     * Returns a post parameter
+     * @param string $id
+     * @return mixed
+     */
     public function getPostParam($id)
     {
         return isset($_POST[$id]) ? $_POST[$id] : null;
     }
 
+    /**
+     * Sets a parameter to $_POST
+     * @param string $id
+     * @param string $value
+     */
     public function setPostParam($id, $value)
     {
         $_POST[$id] = $value;
     }
 
+    /**
+     * Wrapper for setPostParam
+     * @param array $post
+     */
     public function setPostParams(array $post)
     {
         foreach ($post as $key => $value) {
@@ -78,27 +113,39 @@ class Http
         }
     }
 
+    /**
+     * Returns the $_SERVER superglobal
+     * @return array
+     */
     public function getServer()
     {
         return $_SERVER;
     }
 
+    /**
+     * Returns the $_COOKIE superglobal
+     * @return array
+     */
     public function getCookie()
     {
         return $_COOKIE;
     }
 
+    /**
+     * Returns a header
+     * @param string $id
+     * @return string
+     */
     public function getHeader($id)
     {
         $headers = $this->getHeaders();
         return $headers[$id];
     }
 
-    public function setHeader($id, $value)
-    {
-        header($id . ': ' . $value);
-    }
-
+    /**
+     * Returns an the array of headers
+     * @return array
+     */
    public function getHeaders()
     {
         $headers = array();
@@ -109,48 +156,73 @@ class Http
         return $headers;
     }
 
-    public function setHeaders(array $headers)
-    {
-        foreach ($headers as $key => $value) {
-            $this->setHeader($key, $value);
-        }
-    }
-
+    /**
+     * Gets the request method
+     * @return string
+     */
     public function getRequestMethod()
     {
         return $_SERVER['REQUEST_METHOD'];
     }
 
+    /**
+     * Checks whether the request method is get
+     * @return bool
+     */
     public function isGet()
     {
         return $this->getRequestMethod() == 'GET';
     }
 
+    /**
+     * Checks whether the request method is post
+     * @return bool
+     */
     public function isPost()
     {
         return $this->getRequestMethod() == 'POST';
     }
 
+    /**
+     * Checks whether the request method is put
+     * @return bool
+     */
     public function isPut()
     {
         return $this->getRequestMethod() == 'PUT';
     }
 
+    /**
+     * Checks whether the request method is delete
+     * @return bool
+     */
     public function isDelete()
     {
         return $this->getRequestMethod() == 'DELETE';
     }
 
+    /**
+     * Checks whether the request method is head
+     * @return bool
+     */
     public function isHead()
     {
         return $this->getRequestMethod() == 'HEAD';
     }
 
+    /**
+     * Checks whether the request method is options
+     * @return bool
+     */
     public function isOptions()
     {
         return $this->getRequestMethod() == 'OPTIONS';
     }
 
+    /**
+     * Checks whether the request is an ajax request
+     * @return bool
+     */
     public function isAjax()
     {
         return $this->getHeader('X_REQUESTED_WITH') == 'XMLHttpRequest';
--- Response/Http.php
+++ Response/Http.php
@@ -2,7 +2,125 @@
 
 namespace Pollo\Response;
 
+/**
+ * The HTTP implementation of a response
+ */
 class Http
 {
-    
+    /**
+     * The response body
+     * @var string
+     */
+    protected $_body;
+
+    /**
+     * array of HTTP headers
+     * @var array
+     */
+    protected $_headers;
+
+    /**
+     * HTTP response code
+     * @var int
+     */
+    protected $_responseCode = 200;
+
+    /**
+     * Returns the response body
+     * @return string
+     */
+    public function getBody()
+    {
+        return $this->_body;
+    }
+
+
+    /**
+     * Sets the response body
+     * @param string $body
+     */
+    public function setBody($body)
+    {
+        $this->_body = $body;
+    }
+
+    /**
+     * Returns the array of headers
+     * @return array
+     */
+    public function getHeaders()
+    {
+        return $this->_headers;
+    }
+
+    /**
+     * Sets header which will be sent once send() is called
+     * @param string $id
+     * @param string $value
+     */
+    public function setHeader($id, $value)
+    {
+        $this->_headers[$id] = $value;
+    }
+
+    /**
+     * Wrapper for setHeader
+     * @param array $headers
+     */
+    public function setHeaders(array $headers)
+    {
+        foreach ($headers as $key => $value) {
+            $this->setHeader($key, $value)
+        }
+    }
+
+    /**
+     * Returns HTTP response code
+     * @return $code
+     */
+    public function getResponseCode()
+    {
+        return $this->_responseCode;
+    }
+
+    /**
+     * Sets the HTTP response code
+     * @param int $code
+     */
+    public function setResponseCode($code)
+    {
+        if ((int)$code < 100 || (int)$code > 599) {
+            throw new \InvalidArgumentException('The response code must be between 100 and 599');
+        }
+        $this->_responseCode = (int)$code;
+    }
+
+    /**
+     * Appends content after body
+     * @param string $content
+     */
+    public function appendToBody($content)
+    {
+        $this->_body .= $content;
+    }
+
+    /**
+     * Adds content before the body
+     * @param string $content
+     */
+    public function prependToBody($content)
+    {
+        $this->_body = $content . $this->_body;
+    }
+
+    /**
+     * Sets the headers and outputs the response
+     */
+    public function send()
+    {
+        foreach ($this->_headers as $key => $value) {
+            header($id . ': ' . $value);
+        }
+        echo $this->_body;
+    }
 }
